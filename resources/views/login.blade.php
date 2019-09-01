861a4e4cefe03a3e80c03833cdefb9e0128a399e
Tian Khamez <tkhamez@gmail.com>
2019-09-01 19:28:00 +0200

    eb config + text change

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -37,7 +37,7 @@
             <footer class="navbar navbar-dark bg-brave shadow-1">
                 <div class="align-self-center">
                     Brave Collective Services. For support write to support@bravecollective.freshdesk.com or
-                    ask in the ingame channel "Brave IT".
+                    ask in the ingame channel "Brave IT Team".
                 </div>
             </footer>
         </div>
